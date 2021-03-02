96f065ad9dde82bf181698979eefb97f18eb485a
王來遠 <sinosoft@sinosoft.hk>
2021-03-02 16:07:34 +0800

    sleuth 分布式请求链路跟踪

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -59,6 +59,10 @@ public class OrderController {
     return  restTemplate.getForObject(uri+"/payment/lb",String.class);
     }
 
+    @GetMapping("/consumer/payment/zipkin")
+    public String paymentZipkin(){
+        String forObject = restTemplate.getForObject("http://localhost:8001" + "/payment/zipkin", String.class);
+        return forObject;
     }
 
 }
