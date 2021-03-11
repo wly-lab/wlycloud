d270b301ec88dc792c2b55b8922321599939f534
王來遠 <sinosoft@sinosoft.hk>
2021-03-11 17:07:17 +0800

    sentinel 服务限流

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,6 +1,9 @@
 package com.atguigu.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.sun.deploy.security.BlockedException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,4 +24,14 @@ public class FlowLimitController {
         return "--------testB";
     }
 
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHostKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p1",required = false) String p2){
+        return ".......testHotKey";
+    }
+
+    public  String deal_testHostKey(String p1, String p2, BlockedException ex){
+        return "----deal_testHostKey,o(╥﹏╥)o";
+    }
 }
